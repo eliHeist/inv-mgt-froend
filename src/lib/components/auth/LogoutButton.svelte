cf8490dcc5ebfa06f3f9c0ca004978a239457d22
Elijah Triumph <eliyang256@outlook.com>
2025-10-24 20:27:45 +0300

    unauthosized route with logout

---
+++ src/lib/components/auth/LogoutButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { logout } from "$lib/auth"
+	import { goto } from "$app/navigation"
+
+    import { Button } from "$lib/components/ui/button/index"
+
+    async function handleLogout() {
+		await logout()
+		goto("/login")
+	}
+</script>
+
+<Button onclick={handleLogout}>
+    Logout
+</Button>
+
---
+++ src/routes/(auth)/unauthorized/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import LogoutButton from "$lib/components/auth/LogoutButton.svelte"
+
+</script>
+
+<div>
+    <h1>Unauthorized</h1>
+    <p>You do not have permission to access this page.</p>
+    <LogoutButton/>
+</div>
